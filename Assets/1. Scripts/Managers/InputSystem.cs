4c534ef0a9031328e488281c65cc8232ec39e4dc
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2023-11-20 22:48:47 +0200

    WinLoseManager

--- Assets/1. Scripts/Managers/InputSystem.cs
+++ Assets/1. Scripts/Managers/InputSystem.cs
@@ -6,19 +6,24 @@ public class InputSystem : MonoBehaviour
     public Action OnTouchBegan;
     public Action OnTouchEnded;
 
+    [SerializeField] private bool _isUnlocked = true;
+
     void Update()
     {
+        if (_isUnlocked)
+        {
+
 #if UNITY_EDITOR
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
 
-        {
-            OnTouchBegan?.Invoke();
-        }
+            {
+                OnTouchBegan?.Invoke();
+            }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            OnTouchEnded?.Invoke();
-        }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                OnTouchEnded?.Invoke();
+            }
 #else
     if (Input.touchCount > 0)
         {
@@ -38,5 +43,17 @@ public class InputSystem : MonoBehaviour
         }
 #endif
 
+        }
+    }
+
+    public void Lock()
+    {
+        _isUnlocked = false;
+        OnTouchEnded?.Invoke();
+    }
+
+    public void Unlock()
+    {
+        _isUnlocked = true;
     }
 }
--- Assets/1. Scripts/Managers/WinLoseManager.cs
+++ Assets/1. Scripts/Managers/WinLoseManager.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class WinLoseManager : MonoBehaviour
 {
+    [SerializeField] private InputSystem _inputSystem;
 
+    [SerializeField] private GameObject _winWindow;
+    [SerializeField] private GameObject _loseWindow;
+
+    [SerializeField] private Player _player;
+
+    private void Awake()
+    {
+        _loseWindow.SetActive(false);
+        _winWindow.SetActive(false);
+
+        _player.OnDeath += Lose;
+    }
 
     public void Win()
+    {
+        _loseWindow.SetActive(false);
+        _winWindow.SetActive(true);
+
+        _inputSystem.Lock();
+    }
+
+    public void Lose()
+    {
+        _loseWindow.SetActive(true);
+        _winWindow.SetActive(false);
+
+        _inputSystem.Lock();
+    }
+
+    public void NextLevel()
     {
 
     }
 
-    public void Lose()
+    public void RestartLevel()
+    {
+
+    }
+
+    public void Exit()
     {
 
     }
